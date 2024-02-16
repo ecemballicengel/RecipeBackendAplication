246cb29ee3bb337f14115a1a76d86802aa07d7c7
Ecem Ballı Cengel <141059963+ecemballicengel@users.noreply.github.com>
2024-02-16 17:06:52 +0300

    User Tablosuna imageUrl eklendi.

---
+++ Recipe.Dal/Migrations/20240216140425_UserTableImageUrl.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Recipe.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserTableImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Users");
+        }
+    }
+}
