ab69209c745545903ef7a828b326d167a1d3c61b
Ecem Ballı Cengel <141059963+ecemballicengel@users.noreply.github.com>
2024-02-24 00:15:55 +0300

    Filtreleme icin servis yazildi

---
+++ Recipe.Bll/Services/SearchServices/ISearchService.cs
@@ -0,0 +1,11 @@
+﻿using Recipe.Bll.Enums;
+using Recipe.Dtos.Response;
+
+namespace Recipe.Bll.Services.SearchServices
+{
+    public interface ISearchService
+    {
+        List<RecipeResponseDto> GetRecipesByCategory(int id);
+        List<RecipeResponseDto> GetRecipesByCriteria(SearchCriteria search, string keyword);
+    }
+}
---
+++ Recipe.Bll/Services/SearchServices/SearchService.cs
@@ -0,0 +1,79 @@
+﻿using Recipe.Bll.Enums;
+using Recipe.Bll.Services.RecipeServices;
+using Recipe.Dal.DbContexts;
+using Recipe.Dtos.Response;
+
+namespace Recipe.Bll.Services.SearchServices
+{
+    public class SearchService : ISearchService
+    {
+        private readonly RecipeDbContext _dbContext;
+        private readonly IRecipeService _recipeService;
+
+        public SearchService(RecipeDbContext dbContext, IRecipeService recipeService)
+        {
+            _dbContext = dbContext;
+            _recipeService = recipeService;
+        }
+
+        public List<RecipeResponseDto> GetRecipesByCriteria(SearchCriteria search, string keyword)
+        {
+            var response = new List<RecipeResponseDto>();
+            switch (search)
+            {
+                case SearchCriteria.RecipeTitle:
+                    response = GetRecipesByTitle(keyword);
+                    break;
+                case SearchCriteria.RecipeDescription:
+                    response = GetRecipesByRecipeDescriptions(keyword);
+                    break;
+                case SearchCriteria.Ingredients:
+                    response = GetRecipesByIngredients(keyword);
+                    break;
+            }
+
+            return response;
+        }
+
+        private List<RecipeResponseDto> GetRecipesByTitle(string keyword) =>
+            _recipeService.GetRecipeList().Where(x => x.Title.ToLower().Contains(keyword.ToLower())).ToList();
+
+
+        private List<RecipeResponseDto> GetRecipesByRecipeDescriptions(string keyword)
+        {
+            var descriptions = _dbContext.RecipeDescriptions
+                .Where(x => x.IsDeleted == false && x.Description.ToLower().Contains(keyword.ToLower()))
+                .ToList();
+
+            var recipeIdList = new List<int>();
+
+            foreach (var description in descriptions)
+            {
+                recipeIdList.Add(description.RecipeId);
+            }
+
+            return _recipeService.GetRecipeList().Where(x => recipeIdList.Contains(x.Id)).ToList();
+        }
+
+        private List<RecipeResponseDto> GetRecipesByIngredients(string keyword)
+        {
+            var ingredients = _dbContext.RecipeIngredients
+                .Where(x => x.IsDeleted == false && x.Name.ToLower().Contains(keyword.ToLower()))
+                .ToList();
+
+            var recipeIdList = new List<int>();
+
+            ingredients.ForEach(x => recipeIdList.Add(x.RecipeId));
+
+            return _recipeService.GetRecipeList().Where(x => recipeIdList.Contains(x.Id)).ToList();
+
+        }
+
+        public List<RecipeResponseDto> GetRecipesByCategory(int id)
+        => _recipeService.GetRecipeList().Where(x => x.CategoryId == id).ToList();
+
+
+
+
+    }
+}
